2f28b09c749e63e405246288bae13a6db2f24bcc
wuyun1 <842269153@qq.com>
2016-07-01 19:31:07 +0800

    right

--- seckill/WebContent/WEB-INF/jsp/list.jsp
+++ seckill/WebContent/WEB-INF/jsp/list.jsp
@@ -1,5 +1,11 @@
 <html>
 <body>
 <h2>Hello aasdfasdfWorld!</h2>
+
+
+<pre>
+
+</pre>
+
 </body>
 </html>
--- seckill/src/org/seckill/web/SeckillController.java
+++ seckill/src/org/seckill/web/SeckillController.java
@@ -1,10 +1,36 @@
 package org.seckill.web;
 
+import java.util.List;
+
+import org.seckill.entity.Seckill;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/seckill")
 public class SeckillController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private SeckillService seckillService;
+	
+	@RequestMapping(value="/list",method = RequestMethod.GET)
+	public String list(Model model){
+		
+		List<Seckill> list = seckillService.getSeckillList();
+		
+		model.addAttribute("list",list);
+		
+		System.out.println(model);
+		
+		return "list";
+	}
 
 }
